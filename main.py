67c1de18b8a0aac16c58d53840da4a0119ce9cd7
Hwaen <leecy0617@naver.com>
2021-10-05 01:14:18 +0900

    ??

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+import discord
+import asyncio
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+token = os.getenv('token')
+
+client = discord.Client()
+
+#수식어
+item = "?아이템"
+
+####로딩####
+@client.event
+async def on_ready(): 
+    print("디스코드 봇 로그인이 완료되었습니다.")
+    print("디스코드봇 이름:" + client.user.name)
+    print("디스코드봇 ID:" + str(client.user.id))
+    print("디스코드봇 버전:" + str(discord.__version__))
+    print('------')
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("테스트중"))
+
+#####도움말#####
+@client.event
+async def on_message(message):
+    if message.content.startswith("?도움말"):
+        embed=discord.Embed(title="도움말", description="사용 가능한 명령어를 보여드립니다")
+        embed.add_field(name="?도움말", value="사용 가능한 명령어를 가져옵니다.", inline=False)
+        embed.add_field(name="?아이템 (템이름)", value="아이템의 정보를 가져옵니다. EX)?아이템 낭아봉", inline=False)
+        embed.add_field(name="?제작 (템이름)", value="아이템의 제작방법을 가져옵니다. EX)?제작 낭아봉", inline=False)
+        embed.add_field(name="?스킬 (캐릭터 이름) (스킬키)", value="캐릭터의 스킬의정보를 가져옵니다. EX)?스킬 수아 Q", inline=False)
+        embed.add_field(name="?무기스킬 (무기종류)", value="무기스킬의 정보를 가져옵니다. EX)?무기스킬 망치", inline=False)
+        embed.add_field(name="?캐릭터 (캐릭터이름)", value="캐릭터의 정보를 가져옵니다. EX)?캐릭터 수아", inline=False)
+        embed.add_field(name="?패치정보", value="현재 적용된 게임 패치 정보를 알려줍니다.", inline=False)
+        embed.add_field(name="?이메일", value="버그 제보와 문의를 받기 위한 이메일입니다.\n 제목은 [봇 문의]로 통일해주시면 감사합니다.", inline=False)
+        await message.channel.send(embed=embed)
+     
+    if message.content.startswith("?이메일"):
+        await message.channel.send("leecy0617@naver.com 이쪽으로 보내주시면 감사합니다.")
+
+#####아이템#####
+    if message.content.startswith(item):
+         msg = message.content[5:]      
+
+    else:
+        return None
+
+    if msg=="":
+        await message.channel.send("아이템이름을 적어주세요.")
+                
+    
+
+      
+
+client.run(os.getenv("token"))
